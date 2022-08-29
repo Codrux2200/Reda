52feaf7138a018d01584e76bcb7b369bd6e5c5b9
Saad Berrada <saadberrada@MacSaad.local>
2022-08-30 00:20:32 +0100

    push

--- classes/rapport.java
+++ classes/rapport.java
@@ -7,7 +7,8 @@ public class rapport{
     private personnage exchange = new personnage("Aya"); // attribut fixe
     public rapport(){} // constructeur vide;
     
-    public String get(personnage a,personnage b){ // on crée une methode get en public et qui retourne une String
+    public String get(personnage a,personnage b){ // on crée une methode get en public et qui retourne une String comme tu peux le voir
+    // on donne en parametre des objets. comme je te l'ai dit tout est objet en java. donc sa ne derange pas.
         if (b.name.equals("Aya")){ // pour pas avoir a faire des if pour rien. Et comme Aya et la dans tout les cas de figures dans
         // notre exemple alors on va faire en sorte que Aya soit tout le temps dans l'objet A. si elle est dans le B on echange
             b = a;
